9e3ab934eadf63b99fae76d280c5d1ec32c8c741
jana <janahassan2052004@gmail.com>
2026-06-22 22:07:43 +0300

    add missing user_ids to DeviceConfig in unit tests

--- devices/tests/unit/test_sensors.py
+++ devices/tests/unit/test_sensors.py
@@ -14,6 +14,7 @@ def make_config(device_id, device_type):
     return DeviceConfig(
         device_id=device_id,
         user_id="test-user-123",
+        user_ids=["test-user-123"],
         device_name=device_id,
         device_type=device_type,
         location="Test Room",
